85fdd8e60d1ee92f3b1520b67367d51f09abce65
Nestor Marmolejo <nestor.marmolejo@utp.edu.co>
2021-05-11 19:58:59 -0500

    nuevo modulo

--- tulibroenlinea/settings.py
+++ tulibroenlinea/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'withenoise.middleware.WhiteNoiseMiddleware'
+    'withenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'tulibroenlinea.urls'
